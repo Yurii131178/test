d82d7089dcd56fee55664d01c41df3c757c38537
Yurii131178 <yurii.kosenko.uniplyt@gmail.com>
2025-05-07 16:10:38 +0300

    Moved App.tsx to src/components and updated main.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import Product from "./Product";
+
+export default function App() {
+  return (
+    <>
+      <h1>Best selling</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+}
